a658036c807358e882cacb6ba389b0d355bd5389
a22407006DanielPinto <a22407006@alunos.ulht.pt>
2026-06-30 18:28:17 +0000

    Alterações

--- clinica/urls.py
+++ clinica/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns=[
     path('', views.consultas_view, name='consultas_view'),
     path('consulta/criar/', views.criar_consulta_view, name='criar_consulta_view'),
-    path('editar/criar/<int:id>/', views.editar_consulta_view, name='editar_consulta_view'),
-    path('apagar/criar/<int:id>/', views.apagar_consulta_view, name='apagar_consulta_view'),
+    path('consulta/editar/<int:id>/', views.editar_consulta_view, name='editar_consulta_view'),
+    path('consulta/apagar/<int:id>/', views.apagar_consulta_view, name='apagar_consulta_view'),
     path('veterinario/<int:id>/', views.veterinario_view, name='veterinario_view'),
 ]
